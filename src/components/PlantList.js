b74ee766e2abef4ededb22f26baa10f6fbcde8c8
dcho0723 <dcho0723@yahoo.com>
2021-07-28 20:54:43 -0400

    completed assingment, no post

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plantData }) {
+
+  const plantComponent = plantData.map(plant => {
+    return <PlantCard key={plant.id} name={plant.name} image={plant.image} price={plant.price} />
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantComponent}
+      </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantData, setPlantData] = useState([])
+  const [onSearch, setOnSearch] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(resp => resp.json()) 
+    .then(data => setPlantData(data))
+  }, [])
+
+  function searchFunction(searchTerm) {
+    setOnSearch(searchTerm)
+  }
+  const filteredSearch = plantData.filter(plant => plant.name.toLowerCase().includes(onSearch.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlantData={setPlantData}/>
+      <Search searchFunction={searchFunction}/>
+      <PlantList plantData={filteredSearch}/>
     </main>
   );
 }
